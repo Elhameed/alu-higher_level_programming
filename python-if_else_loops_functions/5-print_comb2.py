f9998b436c4a44eebe0e2d28a16fb43e41bd24d0
AbdulhameedAjani <a.ajani@alustudent.com>
2023-02-07 00:56:04 -0800

    new commit

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for num in range(0,100):
     if (num != 99):
-        print("{:02d}" .format(num), end=", " )
+        print("{:02d}, " .format(num), end="")
     else:
         print("{:02d}" .format(num), end="\n")
