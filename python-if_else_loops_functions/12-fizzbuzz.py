21b1039d2a1419a2a9233f9e1cff245f65cce57c
AbdulhameedAjani <a.ajani@alustudent.com>
2023-02-07 22:30:53 -0800

    made pycodestyle correction

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100 +1):
+    for i in range(1, 100 + 1):
         if (i % 3 == 0 and i % 5 == 0):
             print("FizzBuzz", end=" ")
         elif (i % 3 == 0):
