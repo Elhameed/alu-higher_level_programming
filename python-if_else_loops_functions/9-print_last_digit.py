e55d07b6510f4b0ca939e989026419c5562133a5
AbdulhameedAjani <a.ajani@alustudent.com>
2023-02-07 22:02:06 -0800

    added task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = number % 10
+
+    if nummber >= 0:
+        last_digit = number % 10
+    else:
+        last_digit = number % -10
