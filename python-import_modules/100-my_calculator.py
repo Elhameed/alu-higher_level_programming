ba385c2253d6030ad58e032bafeec4ef31db8cb7
AbdulhameedAjani <a.ajani@alustudent.com>
2023-02-28 11:21:30 -0800

    updated task 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    args = sys.argv
+    num_args = len(args)
+
+    if num_args != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(args[1])
+    operator = args[2]
+    b = int(args[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
