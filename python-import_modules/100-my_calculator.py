6d026f806093dc4ffa9b89c136681aca7e07452e
AbdulhameedAjani <a.ajani@alustudent.com>
2023-02-28 11:27:39 -0800

    new commit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+
     args = sys.argv
     num_args = len(args)
 
