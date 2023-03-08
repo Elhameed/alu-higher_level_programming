e3da9cfd1c91942c4a726035ff9be73aa9d8b861
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-08 09:59:15 -0800

    update task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,5 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return None
-    else:
-        return max(my_list)
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return max_value
