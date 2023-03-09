a3cbe11a5680969a202de636bf3baffc1c4cf656
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-09 02:12:02 -0800

    updated task 100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,13 +3,11 @@ def weight_average(my_list=[]):
     # If the list is empty, return 0
     if not my_list:
         return 0
-    
     # Compute the sum of products and the sum of weights
     sum_products = 0
     sum_weights = 0
     for score, weight in my_list:
         sum_products += score * weight
         sum_weights += weight
-    
     # Compute and return the weighted average
     return sum_products / sum_weights
