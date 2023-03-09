0085369c6405f15572eb989cf8ffe59e2a991f34
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-09 02:14:55 -0800

    added task 15

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_to_delete = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del a_dictionary[key]
+    return a_dictionary
