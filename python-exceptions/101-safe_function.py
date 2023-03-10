9c159bdde19af29a38dcfde3226e098a7be52c2a
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-10 01:08:09 -0800

    added task 8

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    else:
+        return result
