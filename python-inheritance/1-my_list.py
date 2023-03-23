8fd3e4a8df2beb341158bb039190e08115b248be
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-22 23:16:57 -0700

    Add print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """prints the sorted list"""
         sorted_list = sorted(self)
