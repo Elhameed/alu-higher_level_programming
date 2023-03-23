85a3246e98f0a07cd738ffc14acfc82967e881ed
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-23 00:03:03 -0700

    Add function inherits_from that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; otherwise False

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""checks if object is an instance of a class
-that inherited
+"""checks if object is an instance of a class that
+inherited from the specified class or not
 """
 
 
 def inherits_from(obj, a_class):
-    """returns true if object is an instance of a class
-    or a class that inherited from the specifed class
+    """Returns true if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False
     """
-    return (issubclass(obj, a_class))
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
