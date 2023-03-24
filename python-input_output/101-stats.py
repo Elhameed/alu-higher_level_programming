27d215b6decc1cb29e0d444ff5530524e154dda8
AbdulhameedAjani <a.ajani@alustudent.com>
2023-03-24 08:57:33 -0700

    Update 101-stats.py to print accumulated metrics without interruption

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,23 +1,15 @@
-#!/usr/bin/python3ads from standard input and computes metrics.
-
-"""
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+#!/usr/bin/python3
+"""Reads from standard input and computes metrics
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+    """Print accumulated metrics
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
