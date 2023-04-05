ba321bde40b1522f4768a5eff3e17904ac3bc730
Elhameed <a.ajani@alustudent.com>
2023-04-05 14:55:13 +0000

    new commit

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line argument
-url="$1"
-
-# Send a request to the URL using curl and save the output to a variable
-response=$(curl -sI $url)
-
-# Extract the Content-Length header from the response
-content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
-
-# Print the size of the body in bytes
-echo $content_length
-
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
