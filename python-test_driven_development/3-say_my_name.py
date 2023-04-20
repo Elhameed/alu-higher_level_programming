87013024286f238852551fe048dd08cdd1d37fe5
AbdulhameedAjani <a.ajani@alustudent.com>
2023-04-20 01:59:57 -0700

    Update 3-say_my_name.py to use boolean expression for last_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,7 +22,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    if last_name != "":
+    if last_name:
         print("My name is {} {}".format(first_name, last_name))
     else:
         print("My name is {}".format(first_name))
