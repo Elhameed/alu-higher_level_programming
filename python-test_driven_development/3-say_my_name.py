4d06af32f3afd3b5f2d640b777bcc05245038314
AbdulhameedAjani <a.ajani@alustudent.com>
2023-04-20 01:57:04 -0700

    Add 3-say_my_name.py & tests, plus 3-main.py to print names

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a name-printing function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the name provided with a message.
+
+    Args:
+        first_name (str): The first name to be printed.
+        last_name (str): The last name to be printed. Defaults to empty string.
+
+    Raises:
+        TypeError: If either `first_name` or `last_name` is not a string.
+
+    Returns:
+        None
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name != "":
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
